8fd48ab4a3a837ad51219166eba3726265c342a6
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2019-11-02 22:19:00 -0300

    Sound is now persistent

--- Assets/IPHAssets/CS_Assets/CS_Scripts/IPHToggleSound.cs
+++ Assets/IPHAssets/CS_Assets/CS_Scripts/IPHToggleSound.cs
@@ -1,15 +1,13 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
-namespace InfiniteHopper
-{
+namespace InfiniteHopper {
     /// <summary>
     /// Toggles a sound source when clicked on. 
     /// In order to detect clicks you need to attach this script to a UI Button and set the proper OnClick() event.
     /// </summary>
-    public class IPHToggleSound : MonoBehaviour
-    {
+    public class IPHToggleSound : MonoBehaviour {
         //The tag of the sound object
         public string soundObjectTag = "GameController";
 
@@ -17,7 +15,7 @@ namespace InfiniteHopper
         public Transform soundObject;
 
         // The index of the current value of the sound
-        internal float currentState = 1;
+        internal float currentState; //## default value now with PlayerPrefs statement in Awake
 
         /// <summary>
         /// Awake is called when the script instance is being loaded.
@@ -27,53 +25,54 @@ namespace InfiniteHopper
         /// Because of this, you should use Awake to set up references between scripts, and use Start to pass any information back and forth. 
         /// Awake is always called before any Start functions. This allows you to order initialization of scripts. Awake can not act as a coroutine.
         /// </summary>
-        void Awake()
-        {
-            if (!soundObject && soundObjectTag != string.Empty) soundObject = GameObject.FindGameObjectWithTag(soundObjectTag).transform;
+        void Awake () {
+            if (!soundObject && soundObjectTag != string.Empty) soundObject = GameObject.FindGameObjectWithTag (soundObjectTag).transform;
 
+            currentState = PlayerPrefs.GetFloat (soundObjectTag, 1);
+            print (soundObjectTag + currentState);
             // Set the sound in the sound source
-            SetSound();
+            SetSound ();
         }
 
         /// <summary>
         /// Sets the sound volume
         /// </summary>
-        void SetSound()
-        {
-            if (!soundObject && soundObjectTag != string.Empty) soundObject = GameObject.FindGameObjectWithTag(soundObjectTag).transform;
+        void SetSound () {
+            if (!soundObject && soundObjectTag != string.Empty) soundObject = GameObject.FindGameObjectWithTag (soundObjectTag).transform;
 
-            Color newColor = GetComponent<Image>().material.color;
+            Color newColor = GetComponent<Image> ().material.color;
 
             // Update the graphics of the button image to fit the sound state
-            if (currentState == 1)
+            if (currentState == 1) //sets the transparency if muted
                 newColor.a = 1;
             else
                 newColor.a = 0.5f;
 
-            GetComponent<Image>().color = newColor;
+            GetComponent<Image> ().color = newColor;
 
             // Set the value of the sound state to the source object
             if (soundObject)
-                soundObject.GetComponent<AudioSource>().volume = currentState;
+                soundObject.GetComponent<AudioSource> ().volume = currentState;
+
+            PlayerPrefs.SetFloat (soundObjectTag, currentState);
         }
 
         /// <summary>
         /// Toggle the sound. Cycle through all sound modes and set the volume and icon accordingly
         /// </summary>
-        void ToggleSound()
-        {
+        void ToggleSound () {
             currentState = 1 - currentState;
 
-            SetSound();
+            SetSound ();
         }
 
         /// <summary>
         /// Starts the sound source.
         /// </summary>
-        void StartSound()
-        {
+        void StartSound () {
             if (soundObject)
-                soundObject.GetComponent<AudioSource>().Play();
+                soundObject.GetComponent<AudioSource> ().Play ();
         }
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jungle-jump
 Desafio Oktagon
+
+
+
+02/11 21:45 -> 
+    Estudando a organizacao do projeto
+    Entendendo a parte do controle do som para implementar o primeiro item
+    
